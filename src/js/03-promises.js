121a4f02140ecf77712fa40d1b297c8b06964646
19Aleksandra95 <polar.heder@gmail.com>
2023-08-20 20:46:03 +0200

    commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,32 +1,38 @@
 import { Notify } from 'notiflix';
+
 function createPromise(position, delay) {
-  const promise = new Promise((resolve, reject) => {
+  const promise = new Promise((result, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
-  return promise;
-}
+   setTimeOut (() => {
+    if (shouldResolve) {
+      result ({position, delay})
+    } else {
+      reject ({position, delay})
+    }
+  }, delay)
+   });
+   
+   return promise;
+} 
+
 const formEl = document.querySelector('.form');
 formEl.addEventListener('submit', onSubmit);
+
 function onSubmit(e) {
   e.preventDefault();
   let delay = Number(e.currentTarget.delay.value);
   const step = Number(e.currentTarget.step.value);
   const amount = Number(e.currentTarget.amount.value);
-  for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, delay).then(onSuccess).catch(onError);
+  for (let i = 1 ; i <= amount ; i += 1) {
+    createPromise(i, delay).then(onSucces).catch(onError);
     delay += step;
   }
 }
-function onSuccess({ position, delay }) {
-  Notify.success(`:white_check_mark: Fulfilled promise ${position} in ${delay}ms`);
+
+function onSucces ({position, delay}) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 }
-function onError({ position, delay }) {
-  Notify.failure(`:x: Rejected promise ${position} in ${delay}ms`);
+
+function onError ({position, delay}) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
